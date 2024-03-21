e6e8b83adc95a8c59f96d027f2e7666e61cfd369
Diego Lopez-Herrera <diego_lopezherrera@yahoo.com>
2024-03-21 18:25:04 -0400

    1st commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,75 @@
+# csv is needed to be imported in order to start
+import os
+import csv
+
+budget_data_path = os.path.join('/Users/diego/Desktop/python_challenge/python-challenge/python-challenge-1/PyBank/Resources/budget_data.csv')
+
+with open(budget_data_path, 'r') as csvfile:
+
+    csv_reader = csv.reader(csvfile)    
+# header is used to read the headers of the list to use
+    header = next(csv_reader)
+
+# Creating the list of the headers
+    Date = []
+    Profit_Losses = []
+
+    for row in csv_reader:
+        Date.append(row[0])
+        Profit_Losses.append(row[1])
+
+# Creating a list for the Profit/Losses column
+prof_loss = [int(value) for value in Profit_Losses]
+
+
+tot_months = len(Date)
+
+# We are adding up all the "Profit/Losses" column to get our total
+total = sum(prof_loss) 
+
+# Important because this is what creates the list of ALL the change in profit/losses
+change = [prof_loss[i + 1] - prof_loss[i] for i in range(len(prof_loss) - 1)]
+
+# Sum change is needed/used in order to add up all the change in profit
+sum_change = sum(change)
+
+# Average change
+ave_change = round(sum_change / len(change), 2)
+
+# Calculating for change in the Greatest Inc(Increase) in Profits
+inc_change = max(change)
+
+# Calculating for change in the Greatest Dec(Decrease) in Profits
+dec_change = min(change)
+
+# Dates for Greatest Inc(Increase) in Profits
+inc_date = Date[change.index(inc_change) + 1]
+
+# Dates for Greatest Dec(Decrease) in Profits
+dec_date = Date[change.index(dec_change) + 1]
+
+
+# Printed into Terminal
+print_results = (
+    "Financial Analysis\n"    
+    "--------------------\n"
+    f"Total Months: {tot_months}\n"
+    f"Total: ${total}\n"
+    f"Average Change: ${ave_change}\n"
+    f"Greatest Inc in Profits: {inc_date}, (${inc_change})\n"
+    f"Greatest Dec in Profits: {dec_date}, (${dec_change})\n"
+)
+print(print_results)
+
+       
+
+# Writes results to Text File
+with open('/Users/diego/Desktop/python_challenge/python-challenge/python-challenge-1/PyBank/Analysis/Results', 'w') as fin_anal:
+    fin_anal.write("Financial Analysis Results\n")
+    fin_anal.write("--------------------\n")
+    fin_anal.write(f"Total Months: {tot_months}\n")
+    fin_anal.write(f"Total: ${total}\n")
+    fin_anal.write(f"Average Change: ${ave_change}\n")
+    fin_anal.write(f"Greatest Inc in Profits: {inc_date}, (${inc_change})\n")
+    fin_anal.write(f"Greatest Dec in Profits: {dec_date}, (${dec_change})\n")
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,73 @@
+# csv is needed to start so we import it.
+import csv
+import os
 
+election_data_path = os.path.join('/Users/diego/Desktop/python_challenge/python-challenge/python-challenge-1/PyPoll/Resources/election_data.csv')
+
+with open(election_data_path, 'r') as csvfile:
+
+    csv_reader = csv.reader(csvfile)
+
+    header = next(csv_reader)
+
+# This is how we are able to go down the row in the candidates column.
+    candidates = [row[2] for row in csv_reader]
+
+# Once we get the last code we need the names.
+name = list(set(candidates))
+
+#Variables to get the end result/names of candidates.
+can_1 = name[0]
+can_2 = name[1]
+can_3 = name[2]
+
+# Allows us to grab/count the votes for each can(candidate).
+can_1_vote = candidates.count(can_1)
+can_2_vote = candidates.count(can_2)
+can_3_vote = candidates.count(can_3)
+
+# To get the total amount of all the votes between each can(candidate).
+tot_votes = len(candidates)
+
+# Here we create a dict(dictionary) and inside the dict is the name
+# of the can(candidate) along with their votes.
+can_votes = {
+    "can_1": 'can_1_vote',
+    "can_2": 'can_2_vote',
+    "can_3": 'can_3_vote'
+}
+
+# Percentage of votes each can(candidate) won. 
+# Formula is simple: we get the votes from each can(candidate)
+# from the can_votes_summary above, then divide their individual
+# votes by the total amount of votes and multpily by 100.
+can_1_perc = round(can_votes[can_1] / tot_votes * 100, 3)
+can_2_perc = round(can_votes[can_2] / tot_votes * 100, 3)
+can_3_perc = round(can_votes[can_3] / tot_votes * 100, 3)
+
+# Time to reveal the the winner of the election
+winner = max(can_votes, key= can_votes.get)
+
+# Election results in Terminal
+print_results = (
+    "Election Results\n"
+    "------------------------\n"
+    f"Total Votes: {tot_votes}\n"
+    f"------------------------\n"
+    f"{can_1}: {can_1_perc}% ({can_1_vote})\n"
+    f"{can_2}: {can_2_perc}% ({can_2_vote})\n"
+    f"{can_3}: {can_3_perc}% ({can_3_vote})\n"
+    "-------------------------\n"
+    f"Winner: {winner}\n"
+)
+# Text file
+with open('/Users/diego/Desktop/python_challenge/python-challenge/python-challenge-1/PyPoll/Analysis/Results', 'w') as elec_res:
+    elec_res.write("Election Results\n")
+    elec_res.write("------------------------\n")
+    elec_res.write(f"Total Votes: {tot_votes}\n")
+    elec_res.write(f"------------------------\n")
+    elec_res.write(f"{can_1}: {can_1_perc}% ({can_1_vote})\n")
+    elec_res.write(f"{can_2}: {can_2_perc}% ({can_2_vote})\n")
+    elec_res.write(f"{can_3}: {can_3_perc}% ({can_3_vote})\n")
+    elec_res.write("-------------------------\n")
+    elec_res.write(f"Winner: {winner}\n")
